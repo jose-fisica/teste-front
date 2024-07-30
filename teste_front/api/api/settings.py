14080be8c3c528bcb47e4aa345b677cf9fa31151
jose_freire <jose@educat.com.br>
2024-07-29 23:52:05 -0300

    .

--- teste_front/api/api/settings.py
+++ teste_front/api/api/settings.py
@@ -1,8 +1,7 @@
 import os
 import datetime
-import django
-from django.utils.encoding import smart_str
-django.utils.encoding.smart_text = smart_str
+
+import health_check  # pylint: disable=unused-import
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
